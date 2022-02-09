7846dd758de4277f78518b981eb45f989f1c1517
carneiroArleilson <carneiroarleilson@gmail.com>
2022-02-09 10:39:56 -0400

    fix outra vez nuild

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './components/home/home';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Home></Home>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
